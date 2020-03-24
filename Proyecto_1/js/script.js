bdf112c3515255e45ce251b43621044fb6f0186b
wmazzilli <wmazzilli@gmail.com>
2020-03-23 19:58:48 -0500

    Proyecto_1

---
+++ Proyecto_1/js/script.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+
+/*
+$( "#respuesta1" ).click(function() {
+let respuesta1 = $("#E").attr("id");
+if(respuesta1 == "E")
+{
+  alert("Ganaste");
+}else{
+    alert("Lo siento sigue intentando");
+}
+});
+$( "#respuesta2" ).click(function() {
+  //let respuesta1 = $("#respuesta1").val();
+  let respuesta2 = $("#J").attr("id");
+  if(respuesta2 == "E")
+  {
+    alert("Ganaste");
+  }else{
+      alert("Lo siento sigue intentando");
+  }
+});
+$( "#respuesta3" ).click(function() {
+  let respuesta3 =  $("#M").attr("id");
+  if(respuesta3 == "E")
+  {
+    alert("Ganaste");
+  }else{
+      alert("Lo siento sigue intentando");
+  }
+*/
+});
+
+
+
+
+
+});
